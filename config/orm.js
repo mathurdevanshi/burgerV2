530ee36638a5e5dce5267c902e27b4eaeef489bc
mathurdevanshi <mathurdevanshi@gmail.com>
2019-08-03 22:02:24 -0700

    got most of the part working for adding in new items with one error

--- config/orm.js
+++ config/orm.js
@@ -19,6 +19,16 @@ var orm = {
         cb(result);
       }
     );
+  },
+
+  create: function(tableInput, val, cb) {
+    connection.query(
+      "INSERT INTO " + tableInput + " (burger_name) VALUES (" + val + ");",
+      function(err, result) {
+        if (err) throw err;
+        cb(result);
+      }
+    );
   }
 };
 
